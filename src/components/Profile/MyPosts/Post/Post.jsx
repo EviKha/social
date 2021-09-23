7b8e8a7eff951af3817f26144c5f5a81b2af2dc1
Evi <evelinna4k@gmail.com>
2021-09-23 15:26:25 +0300

    fix img

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -3,7 +3,7 @@ import post from './Post.module.css'
 const Post = (props) => {
   return (
     <div className={post.post}>
-      <img src= {props.img} />
+      <img src= {props.img} alt='ava'/>
         {props.message}
     <div> <span>♡{props.likeCount}</span></div>
     </div>
